67ebf1e33221e5ad3d301b76b8e5a6abf911f0ce
Ettore Di Giacinto <mudler@gentoo.org>
2018-08-19 14:50:04 +0200

    Change cleanup logic

--- script/post-update.sh
+++ script/post-update.sh
@@ -1,72 +1,5 @@
 #!/bin/bash
 
-PACKAGES_TO_REMOVE=(
-    "x11-themes/hicolor-icon-theme"
-    "gnome-extra/polkit-gnome"
-    "dev-qt/qtcore"
-    "www-client/w3m"
-    "x11-libs/pango"
-    "app-text/mupdf"
-    "x11-libs/libXrandr"
-    "x11-libs/xcb-util"
-    "x11-apps/mkfontdir"
-    "x11-libs/libXi"
-    "x11-misc/shared-mime-info"
-    "x11-misc/util-macros"
-    "x11-themes/gnome-themes-standard"
-    "x11-libs/libXaw"
-    
-    "dev-libs/ppl"
-    "app-admin/sudo"
-    "x11-libs/gtk+:3"
-    "x11-libs/gtk+:2"
-    "dev-db/mariadb"
-    "sys-fs/ntfs3g"
-    "app-accessibility/at-spi2-core"
-    "app-accessibility/at-spi2-atk"
-    "sys-devel/base-gcc:4.7"
-    "sys-devel/gcc:4.7"
-    "net-print/cups"
-    "dev-util/gtk-update-icon-cache"
-    "dev-qt/qtscript"
-    "dev-qt/qtchooser"
-    "dev-qt/qtcore"
-    "app-shells/zsh"
-    "app-shells/zsh-pol-config"
-    "dev-db/mysql-init-scripts"
-    "dev-lang/ruby"
-    "app-editors/vim"
-    "dev-util/gtk-doc-am"
-    "x11-apps/xset"
-    "x11-themes/hicolor-icon-theme"
-    "media-libs/tiff"
-    "media-libs/jbig2dec"
-    "dev-libs/libcroco"
-    "app-text/qpdf"
-    "media-fonts/urw-fonts"
-    "app-text/libpaper"
-    "dev-python/snakeoil"
-    "dev-libs/atk"
-    "dev-perl/DBI"
-    "dev-perl/TermReadKey"
-    "dev-perl/Test-Deep"
-    "virtual/perl-IO-Zlib"
-    "virtual/perl-Package-Constants"
-    "virtual/perl-Term-ANSIColor"
-    "virtual/perl-Time-HiRes"
-    "app-text/asciidoc"
-    "app-text/sgml-common"
-    "virtual/python-argparse"
-    "sys-power/upower"
-    "dev-python/py"
-    "dev-vcs/git"
-    "dev-tcltk/expect"
-    "app-admin/python-updater"
-    "app-portage/eix"
-    "app-portage/gentoolkit"
-    "app-portage/gentoopm"
-)
-
 FILES_TO_REMOVE=(
    "/.viminfo"
    "/.history"
@@ -102,12 +35,14 @@ rsync -av "rsync://rsync.at.gentoo.org/gentoo-portage/licenses/" "/usr/portage/l
 equo up && equo i --nodeps sys-apps/portage sys-apps/entropy app-admin/equo && equo u && \
 echo -5 | equo conf update
 
+PACKAGES_TO_REMOVE=$(equo q list installed -qv)
+
 # Cleanup
 # Handling install/removal of packages specified in env
 for i in "${PACKAGES_TO_REMOVE[@]}"
 do
-	echo "===== Remove $i ====="
-    equo rm --deep --configfiles --force-system "$i"
+    echo "===== Attempt to remove $i ====="
+    equo rm --deep --configfiles "$i"
 done
 
 # Remove compilation tools
